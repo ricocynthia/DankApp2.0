094a082bc80204314aa878070d1654d7dfc9ff50
Cynthia Rico Mendoza <ricomec@augsburg.edu>
2019-12-15 23:32:58 -0600

    Initial Commit

---
+++ DankApp2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DankApp2.0
+//
+//  Created by Rosa Rico Mendoza on 12/15/19.
+//  Copyright © 2019 Cynthia Rico Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
